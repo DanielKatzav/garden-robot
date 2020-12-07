138cf3a00ea45faea91a57b01db5c1f906406296
Daniel Katzav <katzavdaniel@gmail.com>
2020-12-07 21:08:59 +0200

    setup

---
+++ test/test_garden_robot.cpp
@@ -0,0 +1,14 @@
+//
+// Created by daniel on 12/7/20.
+//
+
+// gtest
+#include <gtest/gtest.h>
+
+// Run all the tests that were declared with TEST()
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    srand((int)time(0));
+    return RUN_ALL_TESTS();
+}
